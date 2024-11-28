8b7fce3cd0d00c3d41ba46f93d1bb26b01d6d427
smlee212 <smmylee212@naver.com>
2024-11-28 17:29:55 +0900

    로그 추적기 기능 - 기존 내용

---
+++ advanced/src/test/java/hello/advanced/trace/strategy/TemplateCallBackTest.java
@@ -0,0 +1,35 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.template.CallBack;
+import hello.advanced.trace.strategy.code.template.TimeLogTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateCallBackTest {
+
+    @Test
+    void callbackV1() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(new CallBack() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행");
+            }
+        });
+
+        template.execute(new CallBack() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행");
+            }
+        });
+    }
+
+    @Test
+    void callbackV2() {
+        TimeLogTemplate template = new TimeLogTemplate();
+        template.execute(() -> log.info("비즈니스 로직 1 실행"));
+        template.execute(() -> log.info("비즈니스 로직 2 실행"));
+    }
+}
