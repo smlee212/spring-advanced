a3feeaa18e8bb22b941896bc82ab923de458168e
smlee212 <smmylee212@naver.com>
2024-12-03 19:13:22 +0900

    [적용] 특정 메서드에 대해서만 동적 프록시 적용하기

---
+++ proxy-start/src/main/java/hello/proxy/config/v2_dynamicproxy/DynamicProxyFilterConfig.java
@@ -0,0 +1,43 @@
+package hello.proxy.config.v2_dynamicproxy;
+
+import hello.proxy.app.v1.*;
+import hello.proxy.config.v2_dynamicproxy.handler.LogTraceBasicHandler;
+import hello.proxy.config.v2_dynamicproxy.handler.LogTraceFilterHandler;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Proxy;
+
+@Configuration
+public class DynamicProxyFilterConfig {
+
+    private static final String[] PATTERNS = {"request*", "order*", "save*"};
+
+    @Bean
+    public OrderControllerV1 orderControllerV1(LogTrace trace) {
+        OrderControllerV1 target = new OrderControllerV1Impl(orderServiceV1(trace));
+        OrderControllerV1 proxy = (OrderControllerV1) Proxy.newProxyInstance(OrderControllerV1.class.getClassLoader(),
+                new Class[]{OrderControllerV1.class},
+                new LogTraceFilterHandler(target, trace, PATTERNS));
+        return proxy;
+    }
+
+    @Bean
+    public OrderServiceV1 orderServiceV1(LogTrace trace) {
+        OrderServiceV1 target = new OrderServiceV1Impl(orderRepositoryV1(trace));
+        OrderServiceV1 proxy = (OrderServiceV1) Proxy.newProxyInstance(OrderServiceV1.class.getClassLoader(),
+                new Class[]{OrderServiceV1.class},
+                new LogTraceFilterHandler(target, trace, PATTERNS));
+        return proxy;
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepositoryV1(LogTrace trace) {
+        OrderRepositoryV1 target = new OrderRepositoryV1Impl();
+        OrderRepositoryV1 proxy = (OrderRepositoryV1) Proxy.newProxyInstance(OrderRepositoryV1.class.getClassLoader(),
+                new Class[]{OrderRepositoryV1.class},
+                new LogTraceFilterHandler(target, trace, PATTERNS));
+        return proxy;
+    }
+}
---
+++ proxy-start/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceFilterHandler.java
@@ -0,0 +1,43 @@
+package hello.proxy.config.v2_dynamicproxy.handler;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.util.PatternMatchUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LogTraceFilterHandler implements InvocationHandler {
+
+    private final Object target;
+    private final LogTrace trace;
+    private final String[] patterns;
+
+    public LogTraceFilterHandler(Object target, LogTrace trace, String[] patterns) {
+        this.target = target;
+        this.trace = trace;
+        this.patterns = patterns;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 메서드 이름 필터
+        String methodName = method.getName();
+        if(!PatternMatchUtils.simpleMatch(patterns, methodName)) { // 패턴이 일치하는지 확인
+            return method.invoke(target, args);
+        }
+
+        TraceStatus status = null;
+        try {
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+            status = trace.begin(message);
+            // 로직 호출
+            Object result = method.invoke(target, args);
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
