78823d3c137ebdb6f0bb8f10c695900fee36ef7b
smlee212 <smmylee212@naver.com>
2024-12-03 19:36:48 +0900

    [테스트] cglib를 이용하여 구체 클래스에 대해 동적 프록시 적용하기

---
+++ proxy-start/src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,27 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.service.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+
+    @Test
+    void cglib() {
+        ConcreteService target = new ConcreteService();
+
+        // cglib 를 만드는 코드
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+    }
+}
