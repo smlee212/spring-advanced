c74b394bf62781bc039e2c1a5acab65153514e33
smlee212 <smlee212@gamil.com>
2024-12-05 16:06:13 +0900

    [적용] ProxyFactory 와 Advisor를 통해 CGLIB 프록시 생성

--- proxy-start/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
+++ proxy-start/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -1,7 +1,5 @@
 package hello.proxy.app.v2;
 
-import hello.proxy.app.v1.OrderControllerV1;
-import hello.proxy.app.v1.OrderServiceV1;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Slf4j
 @RequestMapping
 @ResponseBody
-public class OrderControllerV2 implements OrderControllerV1 {
+public class OrderControllerV2 {
 
     private final OrderServiceV2 orderService;
 
