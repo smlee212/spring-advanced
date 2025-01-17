8c786e167326f3c193b6ae84eb1cc0b20d6b9324
smlee212 <smmylee212@naver.com>
2025-01-17 16:28:24 +0900

    JDK 동적 프록시와 CGLIB 프록시 의존 관계 주입 비교

---
+++ aop/src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -0,0 +1,30 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import hello.aop.proxyvs.code.ProxyDIAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+//@SpringBootTest(properties = {"spring.aop.proxy-target-class=false"}) // JDK 동적 프록시
+@SpringBootTest(properties = {"spring.aop.proxy-target-class=true"}) // CGLIB 프록시
+@Import(ProxyDIAspect.class)
+public class ProxyDITest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl; // JDK 동적 프록시를 사용할 때는 의존관계 주입에 실패한다 (인터페이스 기반으로 프록시 생성되기 떄문)
+
+    @Test
+    void go() {
+        log.info("memberService class = {}", memberService.getClass());
+        log.info("memberServiceImpl class = {}", memberServiceImpl.getClass());
+        memberServiceImpl.hello("hello");
+    }
+}
